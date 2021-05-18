aadc3e54b6ff07c496031dbc954d924f44be9d70
Thambolo <84055721+Thambolo@users.noreply.github.com>
2021-05-18 14:13:21 +0800

    frontend update

---
+++ kodo-webserver/kodo-frontEnd/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+  res.sendFile("/public/dashboard.html", { root: __dirname });
+});
+
+/* app.get("/users/:id", (req, res) => {
+  res.sendFile("/public/user.html", { root: __dirname });
+});
+
+app.get("/users/", (req, res) => {
+  res.sendFile("/public/users.html", { root: __dirname });
+});
+
+app.get("/login/", (req, res) => {
+  res.sendFile("/public/login.html", { root: __dirname });
+});
+
+app.get("/register/", (req, res) => {
+  res.sendFile("/public/register.html", { root: __dirname });
+}); */
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Client server has started listening on port ${PORT}`);
+});
