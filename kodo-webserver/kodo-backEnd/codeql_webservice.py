14531fc0b15e5b72f3b09df1e169e52916758beb
Thambolo <84055721+Thambolo@users.noreply.github.com>
2021-05-22 14:43:53 +0800

    updated subprocesses of the .py

--- kodo-webserver/kodo-backEnd/codeql_webservice.py
+++ kodo-webserver/kodo-backEnd/codeql_webservice.py
@@ -1,10 +1,16 @@
 import subprocess
-from typing import Text
 
 # .stout for output
 # .stderr for printing err in python terminal
 # .args for the list of arguments used
 # .returncode '0' means no error
+# cwd argument to change working directory
 
-process1= subprocess.run("dir", shell=True, stdout=subprocess.PIPE, text=True)
+# get the folder name and put in this variable to create DB of it
+folderName = "testFolder"
+
+filePath = f"codeql database create --language=javascript --source-root ./{folderName} ../databases/{folderName}_DB"
+process1= subprocess.run(filePath, shell=True, stdout=subprocess.PIPE, text=True, stderr=subprocess.PIPE, cwd="./kodo-webserver/kodo-backEnd/zipFiles")
+print(process1.returncode)
+print(process1.stderr)
 print(process1.stdout)
