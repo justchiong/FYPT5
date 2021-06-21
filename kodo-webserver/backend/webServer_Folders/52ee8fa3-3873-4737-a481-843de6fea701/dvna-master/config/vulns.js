f07a1fb14cc624cd085614cfe62ca11d900bf774
Thambolo <84055721+Thambolo@users.noreply.github.com>
2021-06-21 15:49:26 +0800

    fix bugs

---
+++ kodo-webserver/backend/webServer_Folders/52ee8fa3-3873-4737-a481-843de6fea701/dvna-master/config/vulns.js
@@ -0,0 +1,14 @@
+module.exports = {
+	'a1_injection': 'A1: Injection',
+	'a2_broken_auth': 'A2: Broken Authentication',
+	'a3_sensitive_data': 'A3: Sensitive Data Exposure',
+	'a4_xxe': 'A4: XML External Entities',
+	'a5_broken_access_control': 'A5: Broken Access Control',
+	'a6_sec_misconf': 'A6: Security Misconfiguration',
+	'a7_xss': 'A7: Cross-site Scripting',
+	'a8_ides': 'A8: Insecure Deserialization',
+	'a9_vuln_component': 'A9: Using Components with Known Vulnerabilities',
+	'a10_logging': 'A10: Insufficient Logging and Monitoring',
+	'ax_csrf': 'A8:2013  Cross-site Request Forgery',
+	'ax_redirect': 'A10:2013 Unvalidated Redirects and Forwards'
+}
